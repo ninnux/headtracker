c3f8ed84365d0c1b68f42577e33a50a7f3883e82
Nino <nino@ninux.org>
2014-09-08 19:46:54 +0200

    pulizia

--- Arduino/Razor_AHRS/Razor_AHRS.ino
+++ Arduino/Razor_AHRS/Razor_AHRS.ino
@@ -529,102 +529,6 @@ encoderBegin(13);
 // Main loop
 void loop()
 {
-  // Read incoming control messages
-  if (Serial.available() >= 2)
-  {
-    if (Serial.read() == '#') // Start of new control message
-    {
-      int command = Serial.read(); // Commands
-      if (command == 'f') // request one output _f_rame
-        output_single_on = true;
-      else if (command == 's') // _s_ynch request
-      {
-        // Read ID
-        byte id[2];
-        id[0] = readChar();
-        id[1] = readChar();
-        
-        // Reply with synch message
-        Serial.print("#SYNCH");
-        Serial.write(id, 2);
-        Serial.println();
-      }
-      else if (command == 'o') // Set _o_utput mode
-      {
-        char output_param = readChar();
-        if (output_param == 'n')  // Calibrate _n_ext sensor
-        {
-          curr_calibration_sensor = (curr_calibration_sensor + 1) % 3;
-          reset_calibration_session_flag = true;
-        }
-        else if (output_param == 't') // Output angles as _t_ext
-        {
-          output_mode = OUTPUT__MODE_ANGLES;
-          output_format = OUTPUT__FORMAT_TEXT;
-        }
-        else if (output_param == 'b') // Output angles in _b_inary format
-        {
-          output_mode = OUTPUT__MODE_ANGLES;
-          output_format = OUTPUT__FORMAT_BINARY;
-        }
-        else if (output_param == 'c') // Go to _c_alibration mode
-        {
-          output_mode = OUTPUT__MODE_CALIBRATE_SENSORS;
-          reset_calibration_session_flag = true;
-        }
-        else if (output_param == 's') // Output _s_ensor values
-        {
-          char values_param = readChar();
-          char format_param = readChar();
-          if (values_param == 'r')  // Output _r_aw sensor values
-            output_mode = OUTPUT__MODE_SENSORS_RAW;
-          else if (values_param == 'c')  // Output _c_alibrated sensor values
-            output_mode = OUTPUT__MODE_SENSORS_CALIB;
-          else if (values_param == 'b')  // Output _b_oth sensor values (raw and calibrated)
-            output_mode = OUTPUT__MODE_SENSORS_BOTH;
-
-          if (format_param == 't') // Output values as _t_text
-            output_format = OUTPUT__FORMAT_TEXT;
-          else if (format_param == 'b') // Output values in _b_inary format
-            output_format = OUTPUT__FORMAT_BINARY;
-        }
-        else if (output_param == '0') // Disable continuous streaming output
-        {
-          turn_output_stream_off();
-          reset_calibration_session_flag = true;
-        }
-        else if (output_param == '1') // Enable continuous streaming output
-        {
-          reset_calibration_session_flag = true;
-          turn_output_stream_on();
-        }
-        else if (output_param == 'e') // _e_rror output settings
-        {
-          char error_param = readChar();
-          if (error_param == '0') output_errors = false;
-          else if (error_param == '1') output_errors = true;
-          else if (error_param == 'c') // get error count
-          {
-            Serial.print("#AMG-ERR:");
-            Serial.print(num_accel_errors); Serial.print(",");
-            Serial.print(num_magn_errors); Serial.print(",");
-            Serial.println(num_gyro_errors);
-          }
-        }
-      }
-#if OUTPUT__HAS_RN_BLUETOOTH == true
-      // Read messages from bluetooth module
-      // For this to work, the connect/disconnect message prefix of the module has to be set to "#".
-      else if (command == 'C') // Bluetooth "#CONNECT" message (does the same as "#o1")
-        turn_output_stream_on();
-      else if (command == 'D') // Bluetooth "#DISCONNECT" message (does the same as "#o0")
-        turn_output_stream_off();
-#endif // OUTPUT__HAS_RN_BLUETOOTH == true
-    }
-    else
-    { } // Skip character
-  }
-
   // Time to read the sensors again?
   if((millis() - timestamp) >= OUTPUT__DATA_INTERVAL)
   {
@@ -636,14 +540,6 @@ void loop()
 
     // Update sensor readings
     read_sensors();
-
-    if (output_mode == OUTPUT__MODE_CALIBRATE_SENSORS)  // We're in calibration mode
-    {
-      check_reset_calibration_session();  // Check if this session needs a reset
-      if (output_stream_on || output_single_on) output_calibration(curr_calibration_sensor);
-    }
-    else if (output_mode == OUTPUT__MODE_ANGLES)  // Output angles
-    {
       // Apply sensor calibration
       compensate_sensor_errors();
     
@@ -654,25 +550,8 @@ void loop()
       Drift_correction();
       Euler_angles();
       
-      //if (output_stream_on || output_single_on) output_angles();
-      if (output_stream_on || output_single_on) my_output_angles(my_yaw_zero);
-    }
-    else  // Output sensor values
-    {      
-      if (output_stream_on || output_single_on) output_sensors();
-    }
-    
-    output_single_on = false;
-    
-#if DEBUG__PRINT_LOOP_TIME == true
-    Serial.print("loop time (ms) = ");
-    Serial.println(millis() - timestamp);
-#endif
+   my_output_angles(my_yaw_zero);
+  
   }
-#if DEBUG__PRINT_LOOP_TIME == true
-  else
-  {
-    Serial.println("waiting...");
-  }
-#endif
+
 }
